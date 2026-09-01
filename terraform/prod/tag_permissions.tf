8d38c256fed5d00ec933ae0175a6a4e55eb27ea6
oscarduque13 <oduque@gjx.ai>
2026-09-01 16:54:41 -0500

    Manage PROD governed tag permissions with Terraform

--- terraform/prod/tag_permissions.tf
+++ terraform/prod/tag_permissions.tf
@@ -24,31 +24,30 @@ data "databricks_tag_policy" "classification" {
 # Después de validar que Terraform funciona correctamente
 # desde GitHub Actions, volveremos a habilitar este bloque.
 #
-# resource "databricks_access_control_rule_set" "classification_permissions" {
-#
-#   name = "accounts/${var.account_id}/tagPolicies/${data.databricks_tag_policy.classification.id}/ruleSets/default"
-#
-#   grant_rules {
-#     principals = [
-#
-#       # Service Principal DEV
-#       "servicePrincipals/d1d0dadf-f5aa-4686-bbaf-213583824bc7",
-#
-#       # Service Principal UAT
-#       "servicePrincipals/ac32d38f-9573-471c-b5c9-b2ff8ec290f6",
-#
-#       # Service Principal PROD
-#       "servicePrincipals/${var.deploy_principal}"
-#     ]
-#
-#     role = "roles/tagPolicy.assigner"
-#   }
-#
-#   grant_rules {
-#     principals = [
-#       "users/mercedesvargas945@gmail.com"
-#     ]
-#
-#     role = "roles/tagPolicy.manager"
-#   }
-# }
+resource "databricks_access_control_rule_set" "classification_permissions" {
+  name = "accounts/${var.account_id}/tagPolicies/${data.databricks_tag_policy.classification.id}/ruleSets/default"
+
+  grant_rules {
+    principals = [
+
+      # Service Principal DEV
+      "servicePrincipals/d1d0dadf-f5aa-4686-bbaf-213583824bc7",
+
+      # Service Principal UAT
+      "servicePrincipals/ac32d38f-9573-471c-b5c9-b2ff8ec290f6",
+
+      # Service Principal PROD
+      "servicePrincipals/${var.deploy_principal}"
+    ]
+
+    role = "roles/tagPolicy.assigner"
+  }
+
+  grant_rules {
+    principals = [
+      "users/mercedesvargas945@gmail.com"
+    ]
+
+    role = "roles/tagPolicy.manager"
+  }
+}
